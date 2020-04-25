8f86a9d6d3a05a671e166676793a94f20e6c5858
Michael Morahan <morahan.mike@gmail.com>
2020-04-25 04:19:58 -0600

    fixed bug

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TextInput, ScrollView, Picker, Slider, Switch, TouchableOpacity, Button, Image, SafeAreaView } from 'react-native';
+import styles from './styles/styles.js';
 
-export default function App() {
+
+function App(){
+  const [outputText, setOutputText] = useState('Ready to get started!?')
+  const [listOfTracks, setListOfTracks] = useState([])
+  // console.log()
+
+  const logText = (input) => {
+    console.log(input)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.Screen}>
+      <View style={styles.head}>
+        <Image source={require('./assets/FL-icon.png')} style={{height: 117, width: 117, alignSelf: "center"}}/>
+        <Text style={styles.Welcome}>Welcome</Text>
+      </View>
+
+    
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
