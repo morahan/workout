a2d5b0a84ce6ca27040b8641b9480b99e91c0440
Michael Morahan <morahan.mike@gmail.com>
2020-04-25 06:27:14 -0600

    save working version of app.js

---
+++ App2.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Text, View, TextInput, ScrollView, Picker, Slider, Switch, TouchableOpacity, Button, Image, SafeAreaView } from 'react-native';
+import styles from './styles/styles.js';
+import Welcome from './components/welcome.js';
+import Workout from './components/workout.js';
+import SetWorkout from './components/setWorkout.js';
+
+function App(){
+
+  return (
+    <SafeAreaView style={styles.Screen}>
+
+      <Welcome></Welcome>
+      {/* <SetWorkout></SetWorkout> */}
+      {/* <WorkOut></WorkOut> */}
+
+    
+    </SafeAreaView>
+  );
+}
+
+export default App;
