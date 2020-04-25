951173d2caaf59173ce3aa195b92bf175fdcc3f6
Michael Morahan <morahan.mike@gmail.com>
2020-04-25 06:29:59 -0600

    draft click num

--- components/welcome.js
+++ components/welcome.js
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
 import { Text, View, TextInput, ScrollView, Picker, Slider, Switch, TouchableOpacity, Button, Image, SafeAreaView } from 'react-native';
 import styles from '../styles/styles.js';
+
+let clickNumber = 0;
+
+function trackClicks() {
+    if (clickNumber === 0) {
+        return <Welcome></Welcome>
+    } else if (clickNumber === 2) {
+        return <SetWorkout></SetWorkout>
+    } else if (clickNumber === 3) {
+        return <Workout></Workout>
+    }
+
+}
+
+
+
 function Welcome() {
     const [welcome, setWelcome] = useState('Welcome');
     const [name, setName] = useState('Get Started')
     const clickHandler = () => {
         setWelcome('Welcome Back!');
-        setName('Michael')
+        setName('Michael');
+        clickNumber++;
+        console.log(clickNumber)
     }
 
     return (
