09ab3dd40f0cc2ea393c995adccebe170326a285
Michael Morahan <morahan.mike@gmail.com>
2020-04-25 05:45:15 -0600

    modified styling

--- styles/styles.js
+++ styles/styles.js
@@ -6,51 +6,66 @@ const styles = StyleSheet.create({
     backgroundColor: '#44ace8',
     alignItems: 'center',
     paddingTop: "10%",
+    justifyContent: 'space-between'
   },
-
+  
   Head: {
-    //: "35%",
+    justifyContent: 'space-between'
+    // flex: 5,
+    // justifyContent: 'space-between',
+  },
+  
+  cta: {
+    color: "#ffff",
+    fontSize: 37,
+    justifyContent: 'space-between'
   },
 
-  Logo: {
-    height: 117, width: 117, alignSelf: "center", paddingTop: 100
+  foot: {
+    // flex: 3,
+    color: "#fff",
+    paddingBottom: "5%"
+    // justifyContent: 'space-between',
   },
+
+  // Logo: {
+  //   height: 117, width: 117, alignSelf: "center", paddingTop: 100
+  // },
 
   Welcome: {
     color: "#ffff",
-    fontSize: 38,
-    textAlign: 'center',
-    textShadowColor: "#000"
+    fontSize: 23,
+    paddingTop: "5%",
+    alignSelf: "center",
   },
 
   LogIn: {
-    marginTop: "10%",
+    margin: "5%",
     backgroundColor: 'whitesmoke',
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 17,
-    height: '25%',
-    width: "50%"
+    height: '10%',
+    width: "75%",
   },
 
   Btn: {
-    paddingTop: 100,
-    backgroundColor: "green",
-    color: "#ffff",
+   justifyContent: "center",
+   borderRadius: 17,
   },
 
-  Text: {
-    color: "#000",
-    fontSize: 17,
-    textAlign: 'center',
-    paddingTop: "15%",
-  },
+  // Text: {
+  //   color: "#000",
+  //   fontSize: 17,
+  //   textAlign: 'center',
+  //   paddingTop: "15%",
+  // },
 
-  Text2: {
-    color: "#000",
-    fontSize: 17,
-    textAlign: 'center',
-  },
+  // Text2: {
+  //   color: "#000",
+  //   fontSize: 17,
+  //   textAlign: 'center',
+  // },
 
   Input: {
     alignItems: 'center',
