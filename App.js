e89518f4851634c69a17dd2ebf9f5efe4e727908
Michael Morahan <morahan.mike@gmail.com>
2020-04-25 05:44:03 -0600

    minimized code

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
 import { Text, View, TextInput, ScrollView, Picker, Slider, Switch, TouchableOpacity, Button, Image, SafeAreaView } from 'react-native';
 import styles from './styles/styles.js';
-
+import Welcome from './components/welcome.js'
 
 function App(){
-  const [outputText, setOutputText] = useState('Ready to get started!?')
-  const [listOfTracks, setListOfTracks] = useState([])
-  // console.log()
 
-  const logText = (input) => {
-    console.log(input)
-  }
   return (
     <SafeAreaView style={styles.Screen}>
-      <View style={styles.head}>
-        <Image source={require('./assets/FL-icon.png')} style={{height: 117, width: 117, alignSelf: "center"}}/>
+
+      <Welcome></Welcome>
+      {/* <View style={styles.Welcome}>
+        <Image source={require('./assets/fl-icon.png')} style={{height: 117, width: 117, alignSelf: "center"}}/>
         <Text style={styles.Welcome}>Welcome</Text>
       </View>
+
+      <View>
+        <Text style={styles.cta}>Get started</Text>
+      </View>
+
+      <View>
+        <Text style={styles.foot}>© Freio Labs, All Rights Reserved</Text>
+      </View> */}
 
     
     </SafeAreaView>
