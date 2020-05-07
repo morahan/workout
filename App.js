fb0ea6c0f54a363ba766124c2040efdf46024359
Michael Morahan <morahan.mike@gmail.com>
2020-05-07 00:34:28 -0600

    organize code

--- App.js
+++ App.js
@@ -1,30 +1,37 @@
 import React, { useState } from 'react';
-// import { Text, View, TextInput, ScrollView, Picker, Slider, Switch, Button, Image, SafeAreaView } from 'react-native';
-// import styles from './styles/styles.js';
-import Welcome from './components/welcome.js';
-import Workout from './components/workout.js';
-import SetWorkout from './components/setWorkout.js';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import 'react-native-gesture-handler';
+import { Text, View, TextInput, ScrollView, Picker, Slider, Switch, Button, Image, SafeAreaView } from 'react-native';
+import styles from './styles/styles.js';
+
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import 'react-native-gesture-handler';
+
+// ==== pages ====
+import Welcome from './pages/welcome.js';
+import Workout from './pages/workout.js';
+// import SetWorkout from './pages/setWorkout.js';
+
+// ==== components =====
+import Footer from './components/footer.js';
 
 const Stack = createStackNavigator();
 function App(){
   return (
-    <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Welcome}/>
-          <Stack.Screen name="Setup" component={SetWorkout} />
-          <Stack.Screen name="Workout" component={Workout} />
-        </Stack.Navigator>
+    // <NavigationContainer>
+    //     <Stack.Navigator>
+    //       <Stack.Screen name="Home" component={Welcome}/>
+    //       <Stack.Screen name="Setup" component={SetWorkout} />
+    //       <Stack.Screen name="Workout" component={Workout} />
+    //     </Stack.Navigator>
+    // </NavigationContainer>
 
-      {/* <SafeAreaView style={styles.Screen}>
+      <SafeAreaView style={styles.Screen}>
         <Welcome></Welcome>
         {/* <SetWorkout></SetWorkout> */}
         {/* <Workout></Workout> */}
-      {/* </SafeAreaView> */}
+        <Footer></Footer>
+      </SafeAreaView>
 
-    // </NavigationContainer>
   );
 }
 
