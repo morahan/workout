4bd44d1dcabb7b216e612d9cbf46e15e993d497d
Michael Morahan <morahan.mike@gmail.com>
2020-05-11 01:27:08 -0600

    successfully update exercise on button click

--- components/exercise.js
+++ components/exercise.js
@@ -1,34 +1,48 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import { Text, View, TextInput, ScrollView, Picker, Slider, Switch, TouchableOpacity, Button, Image, SafeAreaView } from 'react-native';
 import styles from '../styles/styles.js';
+// import { Component } from 'react';
 
-let exerciseList = [["Squats", 25], ["Leg Extensions", 15], ["Lunges", 20]]
+
+
 let count = 0;
+let exerciseList = [["Squats", 25], ["Leg Extensions", 15], ["Lunges", 20]];
+class Exercise extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            // setNum: 1,
+            exercise: exerciseList[count][0],
+            reps: exerciseList[count][1]
+        }
+    }
+    
+    nextExerciseEventHandler = () => {
+        count++;
+        this.setState({
+            exercise: exerciseList[count][0],
+            reps: exerciseList[count][1]
+        });
+        console.log(count)
+    }
 
-function Exercise() {
-    return (
-        <View style={styles.Screen}>
-            <View style={styles.Box}>
-                <View style={styles.EBox}>
-                    <Text style={styles.Text2}>{exerciseList[count][0]}</Text>
-                    <Text style={styles.Text2}>{exerciseList[count][1]}</Text>
-                </View>
-                <View styles={styles.Btn}>
-                    <TouchableOpacity>
-                        <Text style={styles.Text2} onPress={() => count++}>Next Set</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.EBox}>
-                    <Text style={styles.Text2}>Leg Extensions</Text>
-                    <Text style={styles.Text2}>15 Reps</Text>
-                </View>
-                <View style={styles.EBox}>
-                    <Text style={styles.Text2}>Lunges</Text>
-                    <Text style={styles.Text2}>20 Reps</Text>
+    render(){
+        return (
+            <View style={styles.Screen}>
+                <View style={styles.Box}>
+                    <View style={styles.EBox}>
+                        <Text style={styles.Text2}>{this.state.exercise}</Text>
+                        <Text style={styles.Text2}>{this.state.reps}</Text>
+                    </View>
+                    <View styles={styles.Btn}>
+                        <TouchableOpacity>
+                            <Text style={styles.Text2} onPress={this.nextExerciseEventHandler}>Next Set</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
-        </View>
-    );
+        );
+    }
 }
 
 export default Exercise;
