371d7e0d826671651bd31e68c1198198aa8c40bf
Michael Morahan <morahan.mike@gmail.com>
2020-05-12 20:42:51 -0600

    update timer name

--- pages/workout.js
+++ pages/workout.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Text, View, TextInput, ScrollView, Picker, Slider, Switch, TouchableOpacity, Button, Image, SafeAreaView } from 'react-native';
 import styles from '../styles/styles.js';
-import Timer3 from '../components/timer3.js';
+import Timer from '../components/timer.js';
 import Exercise from '../components/exercise.js'
 
 function Workout() {
     return (
         <View style={styles.Screen}>
             <Exercise></Exercise>
-            <Timer3></Timer3>
+            <Timer></Timer>
         </View>
     );
 }
