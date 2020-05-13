53f6deefcf7e2135261f1d37616dc6316ddb2171
Michael Morahan <morahan.mike@gmail.com>
2020-05-12 22:09:03 -0600

    imported styles.js and improved timerStyles - sheet

--- components/timer.js
+++ components/timer.js
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 import CountDown from 'react-native-countdown-component';
 import moment from 'moment';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-
+import styles from '../styles/styles.js';
 
 export default class App extends Component {
     constructor(props) {
@@ -30,23 +30,29 @@ export default class App extends Component {
     render() {
         console.log(this.state.totalDuration);
         return (
-            <View>
+            <View style={timerStyles.container}>
                 <View>
                     <CountDown
                         // background of timer
-                        digitStyle={{backgroundColor: '#44ace8'}}
+                        digitStyle={timerStyles.digitBox}
                         //duration of countdown in seconds
                         until={this.state.totalDuration}
-                        //formate to show
-                        timetoShow={('M', 'S')}
+                        //formate digits to show
+                        timetoShow={['M', 'S']}
+                        // format lables for digits
+                        timeLables={{m: 'Minutes', s: 'Seeeeconds'}}
+                        
+                        style={timerStyles.timer}
                         //on Finish call
                         onFinish={() => alert('Nice Work! Time for next Set')}
                         //on Press call
                         onPress={() => alert('Pump It!')}
-                        // size of component
-                        size={50}
+                        // size of duration Text
+                        size={23}
                         // Play Pause
                         running={this.state.sessionInProgress}
+                        // Digit Text Styling
+                        digitTxtStyle={timerStyles.digitText}
                     />
                 </View>
                 {!this.state.sessionInProgress &&
@@ -67,56 +73,66 @@ export default class App extends Component {
 
 const timerStyles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'whitesmoke',
+        width: "50vw",
+        paddingTop: "5vh",
+        // paddingBottom: "5vh",
     },
+
     timer: {
-        fontSize: 80,
-        textAlign: 'center',
-        margin: 10,
-        height: "10vh",
-        backgroundColor: "green",
+        width: "100%",
+        backgroundColor: "whitesmoke",
+        borderRadius: 17,
+        paddingBottom: "3vh",
+        marginBottom: "3vh",
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+
+    digitBox: {
+        backgroundColor: 'black',
+        marginTop: '3vh',
+        marginTop: '3vh',
+        borderRadius: "100%",
+        width: "10vw",
+        height: "10vw",
     },
+
+    digitText: {
+        backgroundColor: 'black',
+        color: "white",
+        fontSize: 15,
+    },
+
     startButton: {
-        margin: 40,
-        padding: 40,
-        backgroundColor: '#44ace8',
-        width: '80%',
-        alignSelf: 'center',
-        borderRadius: '37px'
+        justifyContent: "center",
+        width: "35vw",
+        height: "7vh",
+        //  padding: "1vh",
+        //  margin: "5vh",
+        //  padding: "15%",
+        //  margin: "10%",
+        justifyContent: "center",
+        alignSelf: "center",
+        backgroundColor: "black",
+        borderRadius: 17,
+        backgroundColor: "green"
     },
+
     stopButton: {
-        margin: 40,
-        padding: 40,
-        backgroundColor: '#44ace8',
-        width: '80%',
-        borderRadius: '37px',
+        justifyContent: "center",
+        width: "35vw",
+        height: "7vh",
+        //  padding: "1vh",
+        //  margin: "5vh",
+        //  padding: "15%",
+        //  margin: "10%",
+        justifyContent: "center",
+        alignSelf: "center",
+        backgroundColor: "black",
+        borderRadius: 17,
     },
+
     colorWhite: {
         textAlign: 'center',
         color: '#fff',
         fontSize: 26
     },
-    header: {
-        position: 'absolute',
-        top: 0,
-        height: 60,
-        width: '100%',
-        backgroundColor: '#44ace8'
-    },
-    headerText: {
-        fontSize: 24,
-        color: '#fff',
-        paddingTop: 15,
-        paddingLeft: 10,
-        textAlign: 'center'
-    },
-
 });
