3f52016052b73cbbbe39a2526df4613c2741798a
Michael Morahan <morahan.mike@gmail.com>
2020-05-25 01:40:47 -0600

    fixed typo

--- pages/welcome.js
+++ pages/welcome.js
@@ -2,23 +2,25 @@ import React, { useState } from 'react';
 import { Text, View, TextInput, ScrollView, Picker, Slider, Switch, TouchableOpacity, Button, Image, SafeAreaView } from 'react-native';
 import styles from '../styles/styles.js';
 
-export default function Welcome({ props }) {
+export default function Welcome({ navigation }) {
     const [welcome, setWelcome] = useState('Welcome');
     
     const clickHandler = () => {
         setWelcome('  Loading...');
-        console.log("props ==== ", props)
+        // console.log("props ==== ", props)
         navigation.navigate('GetStarted');
     }
 
     return (
         <View style={styles.Screen}>
-            <View>
+            <View style={styles.TopLogoAndHeading}>
                 <Image source={require('../assets/fl-icon.png')} style={{ height: 117, width: 117, alignSelf: "center" }} />
                 <Text style={styles.Welcome}>{welcome}</Text>
             </View>
             <View style={styles.LogIn}>
-                <Button style={styles.Btn} title='Get Started' onPress={clickHandler} />
+                <TouchableOpacity style={styles.Btn} onPress={clickHandler} >
+                    <Text style={styles.WText}>Get Started</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
