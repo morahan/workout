714dfd7044928777984b2083ca9a66b7554ea404
Michael Morahan <morahan.mike@gmail.com>
2020-05-25 01:40:06 -0600

    made into class and set up press handlers to nav

--- pages/getStarted.js
+++ pages/getStarted.js
@@ -1,34 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import { Text, View, TouchableOpacity, Button, Image } from 'react-native';
 import styles from '../styles/styles.js';
 
-function GetStarted({ navigation }) {
 
-    return (
-        <View style={styles.Screen}>
-            <View>
-                <Image source={require('../assets/fl-icon.png')} style={{ height: 117, width: 117, alignSelf: "center" }} />
-                <Text style={styles.Welcome}>{welcome}</Text>
-            </View>
-            <View>
-                <Text style={styles.cta}>{name}</Text>
-            </View>
+export default class GetStarted extends Component {
+    constructor(props) {
+        super(props);
+        //initialize the counter duration
+        this.state = {
+            name: 'Your Name',
+        };
+    }
 
-            <View style={styles.LogIn}>
-                <Button style={styles.Btn} title='Sign In' onPress={clickHandler} />
-            </View>
-           
-            <View style={styles.LogIn}>
-                <Button style={styles.Btn} title='Sign Un' onPress={clickHandler} />
-            </View>
+    signInHandler = ({ navigation }) => {
+        this.setState({ name: "Lola Sign In"}),
+        navigation.navigate('SignIn')
+        console.log("Sign In Handler")
+    }
 
-            
+    signUpHandler = () => {
+        this.setState({ name: "Lola Sign Up"})
+        console.log("Sign Up Handlers")
+    }
 
-            <View>
-                <Text style={styles.foot}>© Freio Labs, All Rights Reserved</Text>
+
+    render () {
+        return (
+            <View style={styles.Screen}>
+                <View>
+                    <Image source={require('../assets/fl-icon.png')} style={{ height: 117, width: 117, alignSelf: "center" }} />
+                    <Text style={styles.Head}>Get Started</Text>
+                </View>
+                <View>
+                    <Text style={styles.cta2}>{this.state.name}</Text>
+                </View>
+
+                <View style={styles.LogIn}>
+                    <TouchableOpacity style={styles.Btn} onPress={this.signInHandler}>
+                        <Text style={styles.WText}>Log In</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.Btn} onPress={this.signUpHandler}>
+                        <Text style={styles.WText}>Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
+
+                
+
+                <View>
+                    <Text style={styles.foot}>© Freio Labs, All Rights Reserved</Text>
+                </View>
             </View>
-        </View>
-    );
+        );
+    }
 }
 
-export default GetStarted;
+// export default GetStarted;
