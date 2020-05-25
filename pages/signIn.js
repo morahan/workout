6a8e7bd230ac368d96bf13b9660b48098c93c13e
Michael Morahan <morahan.mike@gmail.com>
2020-05-25 03:20:51 -0600

    refactor to be class
    add conditional render logic

--- pages/signIn.js
+++ pages/signIn.js
@@ -1,40 +1,76 @@
-import React, { useState } from 'react';
-import { Text, View, TextInput, ScrollView, Picker, Slider, Switch, TouchableOpacity, Button, Image, SafeAreaView } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
 import styles from '../styles/styles.js';
 
-// Edit code below! Copied from Welcome 
-function SignIn() {
-    const [welcome, setWelcome] = useState('Welcome');
-    const [name, setName] = useState('Get Started')
-    const clickHandler = () => {
-        setWelcome('Welcome Back!');
-        setName('Michael')
+
+
+export default class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "Sign In Below",
+            name: '',
+            tempName: '',
+            step: 0,
+        };
     }
 
-    return (
-        <View style={styles.Screen}>
-            <View>
-                <Image source={require('../assets/fl-icon.png')} style={{ height: 117, width: 117, alignSelf: "center" }} />
-                <Text style={styles.Welcome}>{welcome}</Text>
-            </View>
-            <View>
-                <Text style={styles.cta}>{name}</Text>
-            </View>
+    pressSignInHandler = () => {
+        this.setState({ message: 'Welcome Back!', name: this.state.tempName, step: 1 });
+    }
 
-            <View style={styles.LogIn}>
-                <Button style={styles.Btn} title='Sign In' onPress={clickHandler} />
-            </View>
+    pressStartHandler = () => {
+        this.props.navigation.navigate('Workout')
+    }
 
-            <View>
-                <Text style={styles.foot}>© Freio Labs, All Rights Reserved</Text>
-            </View>
-        </View>
-    );
+    render () {
+        if (this.state.step === 0){
+            return (
+                <View style={styles.Screen}>
+                    <View style={styles.TopLogoAndHeading}>
+                        <Image source={require('../assets/fl-icon.png')} style={{ height: 117, width: 117, alignSelf: "center" }} />
+                        <Text style={styles.Welcome}>{this.state.message}</Text>
+                    </View>
+                    <View>
+                        <Text style={styles.cta}>{name}</Text>
+                    </View>
+    
+                    <View style={styles.LogIn}>
+                        <TextInput placeholder='username name' style={styles.Input2} onChangeText={text => this.setState({ tempName: text })}></TextInput>
+                        <TextInput placeholder='password' secureTextEntry={true} style={styles.Input2} onChangeText={text => this.setState({ tempName: text })}></TextInput>
+                        <TouchableOpacity style={styles.Btn} onPress={this.pressSignInHandler}>
+                            <Text style={styles.WText}>Sign In</Text>
+                        </ TouchableOpacity>
+                    </View>
+    
+                    <View>
+                        <Text style={styles.foot}>© Freio Labs, All Rights Reserved</Text>
+                    </View>
+                </View>
+            );
+        }
+        if (this.state.step === 1){
+            return (
+                <View style={styles.Screen}>
+                    <View style={styles.TopLogoAndHeading}>
+                        <Image source={require('../assets/fl-icon.png')} style={{ height: 117, width: 117, alignSelf: "center" }} />
+                        <Text style={styles.Welcome}>{this.state.message}</Text>
+                    </View>
+                    <View>
+                        <Text style={styles.cta2}>{this.state.name}</Text>
+                    </View>
+
+                    <View style={styles.LogIn}>
+                        <TouchableOpacity style={styles.Btn} onPress={this.pressStartHandler}>
+                            <Text style={styles.WText}>Start Now</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View>
+                        <Text style={styles.foot}>© Freio Labs, All Rights Reserved</Text>
+                    </View>
+                </View>
+            );
+        }
+    }
 }
-
-export default SignIn;
-
-
-
-
-
