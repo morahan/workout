c19a4313eee0f4dca7de6f45203d6f2b4cbcbbbb
Michael Morahan <morahan.mike@gmail.com>
2020-06-04 21:25:28 -0600

    add func file

---
+++ components/learning/addOrRemove.js
@@ -0,0 +1,10 @@
+const addOrRemove = (array, item) => {
+    const exists = array.includes(item)
+    if (exists) {
+        return array.filter((c) => { return c !== item })
+    } else {
+        const result = array
+        result.push(item)
+        return result
+    }
+}
