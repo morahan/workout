4fd4649f379d47fb97137598409e9e8de769eaf8
Michael Morahan <morahan.mike@gmail.com>
2020-06-06 16:58:17 -0600

    change to stateful component

--- components/learning/display.js
+++ components/learning/display.js
@@ -1,58 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import TagsView from './TagsView.js'
-import { getActiveChildNavigationOptions } from 'react-navigation';
-import Workout from '../workoutList.js'
-import Styles from '../../styles/styles.js'
+import TagsView from './TagsView.js';
+// import { getActiveChildNavigationOptions } from 'react-navigation';
+import Workout from '../workoutList.js';
+import Styles from '../../styles/styles.js';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const selected = ['Today', 'Tomorrow']
-let target = "target"
+const selected = ['Today', 'Tomorrow'];
+let step = 1;
 
-let getTags = {
+const tags = ["one", "two", "Three"];
 
-    getUpperTags: () => { 
+class Display extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            step: 1,
+            target: "target",
+        }
+    }
+    
+    getUpperTags = () => { 
         const num = Object.keys(Workout.upperBody.exercises).length
-        target = "Upper Body";
+        this.setState({
+            target: "Upper Body"
+        })
         for (let i = 1; i < num+1; i++){
             tags.push(Workout.upperBody.exercises[i].name)
         }
-    },
-    getLowerTags: () => { 
-        const num = Object.keys(Workout.lowerBody.exercises).length
-        target = "Lower Body";
+    } 
+    
+    getLowerTags = () => { 
+        const num = Object.keys(Workout.lowerBody.exercises).length;
+        this.setState({
+            target: "Lower Body"
+        })
         for (let i = 1; i < num+1; i++){
             tags.push(Workout.upperBody.exercises[i].name)
         }
-    },
-    getCoreTags: () => { 
+    }
+
+    getCoreTags = () => { 
         const num = Object.keys(Workout.core.exercises).length
-        target = "Core";
+        this.setState({
+            target: "Core"
+        })
         for (let i = 1; i < num+1; i++){
             tags.push(Workout.upperBody.exercises[i].name)
         }
-    },
-}
-            
+    }
+        
 
-const tags = []
+    nextEventHandler = () => {
+        // step++;
+        this.setState({step: this.state.step+=1})
+        console.log("step === ", this.state.step)
+    }
 
-function Display() {
-    getTags.getUpperTags()
-    return (
-        <View style={Styles.Spacer10}>
+    // getUpperTags();
+    
+    render() {
+        return (
+            <>
+                <View style={Styles.ContainerSafeView}>
+                    <View style={Styles.Spacer10}>
+                            {/* <Text style={Styles.Text2}> 
+                            Choose {this.state.target} Exercises 
+                            </Text> */}
+                            <Text style={Styles.Text2}> 
+                            Choose Exercises 
+                            </Text>
+                            <Text>* Note: Not all exercises will be complted in one day. These exercises will be shuffled for you on your {this.state.target} days. </Text>
+                    </View>
+                </View>
 
-            <Text style={Styles.Text2}> 
-            Choose {target} Exercises 
-            </Text>
-            <Text>* Note: Not all exercises will be complted in one day. These exercises will be shuffled for you on your {target} days. </Text>
+                <TagsView
+                    all={tags}
+                    selected={selected}
+                    isExclusive={false}
+                    step={step}
+                />
 
-            <TagsView
-                all={tags}
-                selected={selected}
-                isExclusive={false}
-            />
-        </View>
-    )
+                <View>
+                    <TouchableOpacity style={Styles.BtnBtm}>
+                        <Text style={Styles.WText} 
+                        onPress={this.nextEventHandler}>
+                          Next
+                        </Text>
+                        {/* <Text style={Styles.WText}>Next</Text> */}
+                    </TouchableOpacity>
+                </View>
+            </>
+        );
+    }
 }
 
 export default Display;
