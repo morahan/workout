5746c82f84997797eaaf8c6d7e0dc37bf9589c6e
Michael Morahan <morahan.mike@gmail.com>
2020-07-08 22:20:06 -0600

    created draft of customEx to break down selected
    into muscle groups

--- components/customExercises.js
+++ components/customExercises.js
@@ -1,7 +1,13 @@
-import workout from './workoutList.js';
+// import workout from './workoutList.js';
 
 let customExercises = {
     selected: [],
+//     seperateTags: function(){
+//         let result = [];
+//         this.selected.split(',');
+
+//     }
 };
+console.log("customExercises.js selected === ", customExercises.selected)
 
 export default customExercises;
