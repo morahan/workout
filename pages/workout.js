49ae3e6ef1962ed8e4c2e88273b56def3bfac79a
Michael Morahan <morahan.mike@gmail.com>
2020-07-12 01:05:47 -0600

    added console.log

--- pages/workout.js
+++ pages/workout.js
@@ -4,10 +4,12 @@ import styles from '../styles/styles.js';
 import Exercise from '../components/exercise.js'
 
 function Workout(props) {
-    console.log("workout.js ~ Work Out props === ", props.navigation)
+    console.log("workout.js ~ Work Out props === ", props)
+    console.log("workout.js ~ Work Out props.nav === ", props.navigation)
     return (
         <View style={styles.Screen}>
             <Exercise target={props.navigation.state.params.day}></Exercise>
+            {/* <Exercise></Exercise> */}
             {/* ==== Text below is Proof that we are sending proper target day as props to exercise.js */}
             {/* <Text>{props.navigation.state.params.day}</Text> */}
         </View>
