b0b27eb6b01f4501bb57f772b7b823cf6573faf0
Michael Morahan <morahan.mike@gmail.com>
2020-08-03 20:40:39 -0600

    created example modal for learning

---
+++ pages/modalTest.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import {View, Text, Button, Modal, TouchableOpacity } from 'react-native'
+import styles from '../styles/styles.js';
+
+// let modalVisibility = true;
+
+function ExModal(){
+    const [modalVisibility, setModalVisibility] = useState(false);
+    // render(){
+        return(
+            <View style={{flex: 1, marginTop: 10 }} >
+                <Text style={{fontSize: 48, alignContent: 'center'}}>
+                    This is regular text
+                </Text>
+                <Button title="Turn On Modal" onPress={() => setModalVisibility(true)}/>
+                <Modal 
+                    transpartent={true}
+                    visibility={modalVisibility}
+                >
+                <View style={{backgroundColor: "#000000aa", flex: 1}}>
+                    <View style={{backgroundColor: "#ffffff", margin:50, padding: 25, borderRadius: 37, flex: 1}}>
+                        {/* <TouchableOpacity onPress={() => {modalVisibility = false;}}>
+                            <Text style={styles.HeaderText2}>this is inside the modal</Text>
+                        </TouchableOpacity> */}
+                      {/* ==== Using Hooks === */}
+                        <TouchableOpacity onPress={() => setModalVisibility(false)}>
+                            <Text style={styles.HeaderText2}>this is inside the modal</Text>
+                        </TouchableOpacity>
+                    </View>
+                    
+                </View>
+
+                </Modal>
+            </View>
+        )
+    // }
+}
+
+
+export default ExModal;
