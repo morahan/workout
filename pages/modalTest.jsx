a7250078fd62d7b0ccdeef934a54a4b0d8d1549c
Michael Morahan <morahan.mike@gmail.com>
2020-08-05 23:26:54 -0600

    renamed file and edited

--- pages/modalTest.jsx
+++ pages/modalTest.jsx
@@ -1,57 +1,18 @@
-import React, { useState } from "react";
-import {View, Text, Button, Modal, TouchableOpacity } from 'react-native'
-import styles from '../styles/styles.js';
-import { set } from "react-native-reanimated";
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from '../styles/styles';
 
-// let modalVisibility = true;
-
-function ExModal(){
-    const [modalVisibility, setModalVisibility] = useState(true);
-
-     const openModal = () => {
-        console.log("open modal", modalVisibility);
-        // setModalVisibility(true);
-        <Modal transparent={true} visible={modalVisibility}>
-            <View
-                style={{
-                backgroundColor: "#000000aa",
-                flex: 1,
-                borderRadius: 37,
-                }}
-            >
-                <View
-                style={{
-                    backgroundColor: "#ffffff",
-                    margin: 50,
-                    padding: 25,
-                    borderRadius: 37,
-                    flex: 1,
-                }}
-                >
-                <TouchableOpacity
-                    onPress={() => {
-                    setModalVisibility(false);
-                    }}
-                >
-                    <Text style={styles.HeaderText2}>this is inside the modal</Text>
-                </TouchableOpacity>
-                </View>
+const TestModal = () => {
+    return(
+        <View style={styles.ModalWrapper}> 
+            <View style={styles.ModalBackdrop}>
+                <Text>BackDrop Text</Text>
             </View>
-        </Modal>
-    }
-    
+            <View style={styles.ModalBox}>
+                <Text>This text is inside of Modal Box</Text>
+            </View>
+        </View>
+    )
+};
 
-    return (
-      <View style={{ flex: 1, marginTop: 10, alignItems: "center" }}>
-        <Text style={{ fontSize: 48, alignContent: "center" }}>
-          This is regular text
-        </Text>
-        <TouchableOpacity onPress={() => {openModal()}}>
-          <Text style={styles.HeaderText2}>Turn On Modal</Text>
-        </TouchableOpacity>
-      </View>
-    );
-}
-
-
-export default ExModal;
+export default TestModal;
